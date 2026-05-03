6884faa0abf4c3d34efa57ebbde5644c7cc8f811
tower120 <tower120@gmail.com>
2026-05-03 13:14:22 +0300

    MIRI support for rust >= 1.95

--- src/bitset/serde.rs
+++ src/bitset/serde.rs
@@ -13,18 +13,18 @@ impl<Conf: Config> Serialize for BitSet<Conf>{
         S: serde::Serializer
     {
         use arrayvec::ArrayVec;
-        
+
         // SBO
         const STACK_BUFFER_LEN: usize = 4096;
         let mut on_stack: ArrayVec<u8, STACK_BUFFER_LEN>;
         let mut on_heap : Vec<u8>;
-        
+
         // real_len <= approx_len
-        let approx_len = 
+        let approx_len =
             Conf::DataBitBlock::size()                                               // root block
             + (1 + self.level0.mask().count_ones()) * Conf::Level1BitBlock::size()   // lvl1 blocks
             + (1 + self.data.blocks().len())        * Conf::DataBitBlock::size();    // approx data blocks
-        
+
         // There should be no errors at all.
         let array = if approx_len <= STACK_BUFFER_LEN {
             on_stack = ArrayVec::new();
@@ -35,7 +35,7 @@ impl<Conf: Config> Serialize for BitSet<Conf>{
             unsafe{ self.serialize(&mut on_heap).unwrap_unchecked(); }
             on_heap.as_slice()
         };
-        
+
         if serializer.is_human_readable() {
             // collect_str instead of serialize_str allow to omit constructing
             // intermediate base64 encoded String.
@@ -54,7 +54,7 @@ impl<'de, Conf: Config> Deserialize<'de> for BitSet<Conf>{
         D: serde::Deserializer<'de>
     {
         use serde::de::Error;
-        
+
         if deserializer.is_human_readable() {
             struct Visitor<Conf>(PhantomData<Conf>);
             impl<'de, Conf: Config> serde::de::Visitor<'de> for Visitor<Conf> {
@@ -75,7 +75,7 @@ impl<'de, Conf: Config> Deserialize<'de> for BitSet<Conf>{
             struct Visitor<Conf>(PhantomData<Conf>);
             impl<'de, Conf: Config> serde::de::Visitor<'de> for Visitor<Conf> {
                 type Value = BitSet<Conf>;
-            
+
                 fn expecting(&self, formatter: &mut Formatter) -> fmt::Result {
                     formatter.write_str("a byte slice")
                 }
@@ -83,7 +83,7 @@ impl<'de, Conf: Config> Deserialize<'de> for BitSet<Conf>{
                 fn visit_bytes<E: Error>(self, v: &[u8]) -> Result<Self::Value, E> {
                     BitSet::deserialize(&mut Cursor::new(v)).map_err(Error::custom)
                 }
-            }            
+            }
             deserializer.deserialize_bytes(Visitor(PhantomData))
         }
     }
@@ -95,7 +95,7 @@ mod tests {
     use itertools::assert_equal;
     use crate::config;
     use super::*;
-    
+
     #[test]
     fn simple_serde_json_test(){
         let mut bitset: BitSet<config::_64bit> = Default::default();
@@ -105,14 +105,14 @@ mod tests {
 
         let serialized = serde_json::to_string(&bitset).unwrap();
         println!("Serialized {:?}", serialized);
-        
+
         let deserialized_bitset: BitSet<config::_64bit> = serde_json::from_str(&serialized).unwrap();
         println!("Deserialized {:?}", deserialized_bitset);
-        
+
         assert_eq!(bitset, deserialized_bitset);
         assert_equal(bitset.iter(), deserialized_bitset.iter());    // check by iter too.
     }
-    
+
     #[test]
     fn simple_serde_bincode_test(){
         let mut bitset: BitSet<config::_64bit> = Default::default();
@@ -123,24 +123,24 @@ mod tests {
         let config = bincode::config::standard();
         let serialized = bincode::serde::encode_to_vec(&bitset, config).unwrap();
         println!("Serialized {:?}", serialized);
-        
+
         let deserialized_bitset: BitSet<config::_64bit> = bincode::serde::decode_from_slice(&serialized, config).unwrap().0;
         println!("Deserialized {:?}", deserialized_bitset);
-        
+
         assert_eq!(bitset, deserialized_bitset);
         assert_equal(bitset.iter(), deserialized_bitset.iter());    // check by iter too.
     }
-    
+
     #[test]
     fn serde_json_w_file() -> anyhow::Result<()> {
         type BitSet = crate::BitSet<config::_64bit>;
         let set = BitSet::from_iter(0..260_000usize);
-        
+
         let mut file = tempfile::tempfile()?;
         serde_json::to_writer(&mut file, &set)?;
-        
+
         file.rewind()?;
-        let _s: BitSet = serde_json::from_reader(file)?;
+        let _: BitSet = serde_json::from_reader(file)?;
         Ok(())
-    }    
+    }
 }
